dbe2612b898eb1e85fd7d239187c39fe41201c20
latifbaraba <latifbaraba88@gmail.com>
2021-03-17 20:32:54 +0700

    First commit

---
+++ src/data/customizer/config.js
@@ -0,0 +1,24 @@
+export class ConfigDB {
+	static data = {
+		settings: {
+			layout_type: 'ltr',
+		sidebar: {
+			type: '',
+			body_type: '',
+		},
+			sidebar_setting: 'default-sidebar',
+			sidebar_background: 'dark-sidebar' 
+				   },
+			color: {
+			layout_version: 'light',
+			color: 'color-2',
+			primary_color: '#0288d1',
+			secondary_color: '#26c6da',
+			mix_layout: 'dark-only',
+				},
+			router_animation: 'fadeIn'
+		
+	}
+}
+
+export default ConfigDB;
